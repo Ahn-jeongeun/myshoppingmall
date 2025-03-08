4ea756e483e3caac0430c4d173858f29fb349600
DESKTOP-5R2ILP7\jn643 <Ahn-jeongeun>
2025-03-08 23:12:06 +0900

    membercontrol

--- src/main/java/com/shop/cafe/controller/MemberController.java
+++ src/main/java/com/shop/cafe/controller/MemberController.java
@@ -1,14 +1,28 @@
 package com.shop.cafe.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shop.cafe.dto.Member;
+import com.shop.cafe.service.MemberService;
+
 @RestController
 public class MemberController {
+	@Autowired
+	MemberService memberService;
 	
-//	public String insertMember(@RequestBody Member m) {
-//		memberService.insertMember(m);
-//		return "ok";
-//}
+	@PostMapping("insertMember")
+	public String insertMember(@RequestBody Member m) {
+		try {
+			memberService.insertMember(m);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return e.getMessage();
+		}
+		return "ok";
+}
 
 }
--- src/main/java/com/shop/cafe/dao/MemberDao.java
+++ src/main/java/com/shop/cafe/dao/MemberDao.java
@@ -1,12 +1,49 @@
 package com.shop.cafe.dao;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import com.shop.cafe.dto.Member;
+
+@Repository
 public class MemberDao {
+	@Value("${spring.datasource.driver-class-name}")
+	private String DB_DRIVER;
 	
-	public void insertMember(Member m) {
+	@Value("${spring.datasource.url}")
+	private String DB_URL;
+	
+	@Value("${spring.datasource.username}")
+	private String DB_USER;
+	
+	@Value("${spring.datasource.password}")
+	private String DB_PW;
+	
+	public void insertMember(Member m) throws Exception {
+		String sql = "insert into member(type, id, pwd, name, address, phone, email) values (?,?,?,?,?,?,?)";
 		Class.forName(DB_DRIVER);
-		Connection con = DriverManager.getConnection())
+		try (
+				Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PW);
+				PreparedStatement stmt = con.prepareStatement(sql);)
+			
+		{
+			stmt.setString(1, m.getType());
+			stmt.setString(2, m.getId());
+			stmt.setString(3, m.getPwd());
+			stmt.setString(4, m.getName());
+			stmt.setString(5,  m.getAddress());
+			stmt.setString(6, m.getPhone());
+			stmt.setString(7, m.getEmail());
+			int i = stmt.executeUpdate();
+			System.out.println(i+"행이 insert 되었습니다");
+		}
+		
+		
+		
 	}
 
 }
